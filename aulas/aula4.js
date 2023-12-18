37f8f1949bd31b85c80e0b4d384e06be6f42a9b2
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-18 12:12:52 -0300

    criei aula 4 de JS e html

---
+++ aulas/aula4.js
@@ -0,0 +1,21 @@
+let num = 10
+let num2 = 20
+
+//aritiméticos:
+
+num = num+3 //adicionou 3 (passa a valer 13).
+num = num-1 //retirou 1 (passa a valer 12).
+num = num*2 //multiplicou por 2 (passa a valer 24).
+num = num/2 //dividiu por 2 (passa a valer 12).
+//(num = num%3) o valor da variável passaria a ser o resto a divisão por 2, que é 0.
+console.log(num)
+
+//atribuição:
+
+num2++ //incrementa 1 ao valor da variável (passa a valer 21).
+num2-- //decrementa 1 do valor total da variável (volta a valer 20).
+num2+=5 //incrementa 5 ao valor original da variável (passa a vaer 25).
+num2-=10 // decrementa 10 do valor total da variável (passa a valer 15).
+num2*=3 //multiplica o valor da váriavel por 3 (passa a valer 45).
+num2/=2 //divide o valor da variável por 2 (22.2).
+console.log(num2)
