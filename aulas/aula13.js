182692e58619a2e46d0668e9fa2b5a62bb268bda
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-19 10:41:37 -0300

    criei exercícios da aula 13

---
+++ aulas/aula13.js
@@ -0,0 +1,11 @@
+let num=10
+
+if(num >=10) {
+    console.log("número maior que dez.")
+} else if(num >5){
+    console.log("número está entre 5 e 10")
+} else{
+    console.log("numeral menor ou igual a 5.")
+}
+
+console.log("fim do programa.")
