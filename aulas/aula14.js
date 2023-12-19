52216139fdf207b17b0286972aba600bd176d97b
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-19 10:41:47 -0300

    criei exercícios da aula 14

---
+++ aulas/aula14.js
@@ -0,0 +1,20 @@
+let  colocacao=5
+
+
+switch(colocacao) {
+    case 1:
+        console.log("primeiro lugar")
+        break
+    case 2:
+        console.log("segundo lugar")
+        break
+    case 3:
+        console.log("terceiro lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("prêmio de participação")
+        break
+    default: 
+        console.log("não subiu ao pódio")
+        break
+}
