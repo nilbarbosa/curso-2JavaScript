bdc5add105523d299c2c7de7cc2bca661b3e2055
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-19 11:06:53 -0300

    criei aula 15

---
+++ aulas/aula15.js
@@ -0,0 +1,8 @@
+console.log("inicio do programa.")
+
+for(let i=0; i < 10; i++) {
+    console.log("cfb cursos")
+}
+//a variável "i" vai recebendo +1 até o valor ser maior que 10.
+
+console.log("fim do programa.")
