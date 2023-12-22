ac9b619ae3645597be4c34bfe534927772536623
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-22 15:29:22 -0300

    criei exercicios da aula 20

---
+++ aulas/aula20.js
@@ -0,0 +1,30 @@
+/* function nome() { //declaração da função
+    console.log("cfb cursos")
+}
+
+for(let i=0; i<10; i++) {
+    nome()
+} //chamada da função
+*/
+//------------------------------------------//
+/*
+function soma2_10() {
+    let n1=2
+    let n2=10
+    let soma=n1+n2
+    console.log(soma)
+}
+for(let i=0;i<10;i++)
+soma2_10()
+*/
+//-----------------------------------------//
+
+function mudarTexto() {
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML = ("cfb cursos")
+    d2.innerHTML = ("cfb cursos")
+    d3.innerHTML = ("cfb cursos")
+
+}
