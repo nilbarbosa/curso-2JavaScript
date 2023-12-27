f679b4e951c6d9dbe328a68594df7d7d927dd144
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-26 22:19:30 -0300

    finalizei aula 20

--- aulas/aula20.7.js
+++ aulas/aula20.7.js
@@ -1,10 +1,20 @@
 function* perguntas(){
     const nome=yield 'Qual seu nome?'
     const esporte=yield 'Qual o seu esporte favorito?'
-    return 'seu nome é ' + nome + 'seu esporte favorito é ' + esporte
-}
+    return 'seu nome é ' + nome + ' seu esporte favorito é ' + esporte + '.'
+} 
 
 const itp=perguntas()
 console.log(itp.next().value)
 console.log(itp.next('bruno').value)
 console.log(itp.next('natação').value)
+
+function* contador() {
+    let i=0
+    while(true) {
+        yield i++
+    }
+} 
+const itc=contador()
+
+console.log(itc.next().value)
