cf5116329b96d4d76139c7f8e779eb6a5ebbe0d9
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-29 18:48:51 -0300

    crirei exercicios da aula 26

---
+++ aulas/aula26.js
@@ -0,0 +1,18 @@
+const divTodos=[...document.getElementsByTagName("div")]
+const cursosTodos=[...document.getElementsByClassName("cursos")]
+const cursosc1=[...document.getElementsByClassName("cursos")]
+const cursosc2=[...document.getElementById("c1")]
+
+const quarry_divTodas=document.querySelectorAll("div")
+const quarry_cursosTodas=document.querySelectorAll("div")
+
+console.log(quarry_divTodas)
+
+/* console.log(divTodos)
+console.log(cursosTodos)
+console.log(cursosc1)
+console.log(cursosc2) */
+
+/* cursosc1.map((el)=>{
+    el.classList.add("destaque")
+}) */
