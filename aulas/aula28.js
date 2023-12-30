d1360e732e90144a62a1816e5b1b6902df750f3e
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-30 10:20:39 -0300

    criei exercicios da aula 28

---
+++ aulas/aula28.js
@@ -0,0 +1,25 @@
+const caixa1=document.getElementById("caixa1")
+const caixa2=document.getElementById("caixa2")
+const btn=document.getElementById("btn_transferir")
+const todosCursos=[...document.querySelectorAll(".curso")]
+
+
+todosCursos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const curso=evt.target
+       curso.classList.toggle("selecionado")
+    })
+})
+
+btn.addEventListener("click",()=>{
+    const cursosSelecionados=[...document.querySelectorAll(".selecionado")]
+    const cursosNaoSelecionados=[...document.querySelectorAll(".curso:not(.selecionado)")]
+    cursosSelecionados.map((el)=>{
+        caixa2.appendChild(el)
+    })
+
+    cursosNaoSelecionados.map((el)=>{
+        caixa1.appendChild()
+    })
+    //console.log(cursosSelecionados)
+})
