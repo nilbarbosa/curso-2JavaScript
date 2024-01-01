a484b3c6790e740f1201b65f3c31d1a99887d97b
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-01 16:29:10 -0300

    finalizei  exercícios da aula 30

--- aulas/aula30.js
+++ aulas/aula30.js
@@ -1,3 +1,17 @@
 const caixa1=document.querySelector("#caixa1")
+const btn_c=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
 
-console.log(caixa1.children)
+console.log(c1_2.parentElement)
+
+/* console.log(caixa1.hasChildNodes())
+console.log(btn_c[0].hasChildNodes())
+console.log(btn_c[0].childNodes)
+
+console.log(btn_c[0].children.length > 0 ? "possui filhos." : "não possui filhos.")
+
+console.log(caixa1.children[0].innerHTML="TESTE")
+ */
+//console.log(caixa1.firstChild)
+//console.log(caixa1.lastChild)
+//console.log(caixa1.replaceChildren)
