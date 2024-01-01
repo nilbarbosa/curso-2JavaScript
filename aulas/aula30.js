ffedc20b124bde920be03720d36b227aae53f6cf
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-01 17:17:36 -0300

    reorganizei os exercícios da aula 30

--- aulas/aula30.js
+++ aulas/aula30.js
@@ -1,10 +1,11 @@
+
 const caixa1=document.querySelector("#caixa1")
 const btn_c=[...document.querySelectorAll(".curso")]
 const c1_2=document.querySelector("#c1_2")
 
 console.log(c1_2.parentElement)
 
-/* console.log(caixa1.hasChildNodes())
+/* console.laulas/aula30.jsog(caixa1.hasChildNodes())
 console.log(btn_c[0].hasChildNodes())
 console.log(btn_c[0].childNodes)
 
