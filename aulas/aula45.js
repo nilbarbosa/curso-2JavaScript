44841968612ffeefe94378e81eabfde1a9718d37
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-03 22:13:30 -0300

    comentei conteúdo do exercício 45

--- aulas/aula45.js
+++ aulas/aula45.js
@@ -1,9 +1,12 @@
 const caixa=document.querySelector("#caixa")
 
 let mapa=new Map()
+//permite a variavel "mapa" usar o Map.
 
 mapa.set("curso","javaScript")
+//recebe a chave primeiro, e o valor depois.
 mapa.set(10,"cfb cursos")
+//pode variar os tipos, tanto da chave quanto do valor.
 mapa.set(1,100)
 mapa.set("canal",100)
 
