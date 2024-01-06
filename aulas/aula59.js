15e60b1ea29d86cb821b988b8382f4d96c566832
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-05 23:20:54 -0300

    criei exercicio de calculadora referente a aula 59

---
+++ aulas/aula59.js
@@ -0,0 +1,59 @@
+const teclasNum=[...document.querySelectorAll(".num")]
+const teclasOp=[...document.querySelectorAll(".op")]
+const teclasRes=document.querySelector(".res")
+const display=document.querySelector(".display")
+const teclaOn=document.getElementById("tOn")
+const teclaLimpar=document.getElementById("tLimpar")
+const teclaIgual=document.getElementById("tIgal")
+
+let sinal=false
+let decimal=false
+
+teclasNum.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        sinal=false
+        if(evt.target.innerHTML==","){
+            if(!decimal){
+            decimal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML="0,"
+            } else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+            }
+        } else{
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            display.innerHTML+=evt.target.innerHTML
+        }
+    })
+})
+teclasOp.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if(!sinal){
+            sinal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            if(evt.target.innerHTML=="x"){
+                display.innerHTML+="*"
+            } else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+        }
+    })
+})
+
+teclaLimpar.addEventListener("click",()=>{
+    sinal=true
+    decimal=false
+    display.innerHTML=0
+})
+
+teclaIgual.addEventListener("click",()=>{
+    sinal=false
+    decimal=false
+    const res=eval(display.innerHTML)
+    display.innerHTML=res
+})
