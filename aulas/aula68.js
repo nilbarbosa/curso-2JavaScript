cfa204cb5a80e00aeb09ef4e65cac498e8d56e8f
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-08 20:39:09 -0300

    criei exercicios de prática referentes a aula 68

---
+++ aulas/aula68.js
@@ -0,0 +1,110 @@
+const palco=document.getElementById("palco")
+const num_object=document.getElementById("num_object")
+const txt_qtde=document.getElementById("txt_qtde")
+const btn_add=document.getElementById("btn_add")
+const btn_remover=document.getElementById("btn_remover")
+
+
+let larguraPalco=palco.offsetWidth
+let alturaPalco=palco.offsetHeight
+let  bolas=[]
+let numBola=0
+
+class Bola{
+    constructor(arrayBolas,palco){
+        this.tam=Math.floor(Math.random()*10)+10
+
+        this.palco = palco
+        
+        this.r=Math.floor(Math.random()*255)
+        this.g=Math.floor(Math.random()*255)
+        this.b=Math.floor(Math.random()*255)
+        this.px=Math.floor(Math.random()*(larguraPalco-this.tam))
+        this.py=Math.floor(Math.random()*(alturaPalco-this.tam))
+        this.velx=Math.floor(Math.random()*2)+0.5
+        this.vely=Math.floor(Math.random()*2)+0.5
+        this.dirx=(Math.random()*10)>5?1:-1
+        this.diry=(Math.random()*10)>5?1:-1
+        this.palco=this.palco
+        this.arrayBolas=this.arrayBolas
+        this.id=Date.now()+"_"+Math.floor(Math.random()*100100100100)
+        this.desenhar()
+        this.controle=setInterval(this.controlar,10)
+
+        this.eu=document.getElementById(this.id)
+        numBola++
+        num_object.innerHTML=numBola
+    }
+    minhaPos=()=>{
+        return this.arrayBolas.indexOf(this)
+    }
+
+    remover=()=>{
+        clearInterval(this.controle)
+        this.bolas=bolas.filter((b)=>{
+            if(b.id!=this.id){
+                return b
+            }
+        })
+        this.eu.remove()
+        numBola--
+        num_object.innerHTML=numBola
+    }
+    desenhar = () => {
+        const div = document.createElement("div");
+        div.setAttribute("id", this.id);
+        div.setAttribute("class", "bola");
+        div.setAttribute(
+          "style",
+          `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`
+        );
+        this.palco.appendChild(div);
+      };
+
+    controle_bordas=()=>{
+        if(this.px+this.tam >= larguraPalco){
+            this.dirx=-1
+        } else if(this.px <= 0){
+            this.dirx=1
+        }
+        if(this.py+this.tam >= alturaPalco){
+            this.diry=-1
+        } else if(this.py <= 0){
+            this.diry=1
+        }
+    }
+    
+    controlar = () => {
+        this.controle_bordas();
+        this.px += this.dirx * this.velx;
+        this.py += this.diry * this.vely;
+        this.eu.setAttribute(
+          "style",
+          `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`
+        );
+        this.palco.appendChild(this.eu);
+        if (this.px > larguraPalco || this.py > alturaPalco) {
+          this.remover();
+        }
+      };
+    }    
+
+window.addEventListener("resize",(evt)=>{
+    larguraPalco=palco.offsetWidth
+    alturaPalco=palco.offsetHeight
+    console.log(larguraPalco)
+})
+
+btn_add.addEventListener("click",(evt)=>{
+    const qtde=txt_qtde.value
+    for(let i=0; i<qtde;i++){
+       bolas.push(new Bola(bolas,palco))
+        //instanciar novas bolinhas
+    }
+})
+
+btn_remover.addEventListener("click",(evt)=>{
+    bolas.map((b)=>{
+        b.remover()
+    })
+})
