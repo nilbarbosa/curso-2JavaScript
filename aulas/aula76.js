81759d2c51b299d04cd079aca20e0eacbe65c976
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-09 22:01:22 -0300

    criei exercícios referentes a aula 76

---
+++ aulas/aula76.js
@@ -0,0 +1,8 @@
+
+import getTodosCursos from "./teste3.js";
+//import { cursos as c,getCurso as gc} from "./teste3.js"; //renomeando os importados
+import * as m_cursos from "./teste3.js"
+
+console.log(m_cursos.cursos)
+console.log(m_cursos.getCurso())
+//console.log(gc(0)) 
---
+++ aulas/teste2.js
@@ -0,0 +1,13 @@
+const cursos=["javaScript","HTML","CSS", "Ardumuo","C++","C#"]
+const carros=["polo","Tcross","Dix","Argo","Cromos"]
+
+export default function getTodosCurso(){
+    return cursos
+}
+//indica a função padrão
+
+ function getCurso(i_curso){
+    return cursos[i_curso]
+}
+
+export {cursos}
