4ef150c93f4425cf86e709ab7d746bb1569f2e1b
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-09 22:01:07 -0300

    criei exercícios referentes a aula 75

---
+++ aulas/aula75.js
@@ -0,0 +1,4 @@
+import getTodosCurso,{ cursos } from "./teste2.js";
+
+console.log(cursos)
+console.log(getTodosCurso)
---
+++ aulas/teste.js
@@ -0,0 +1,4 @@
+const cursos=["javaScript","HTML","CSS", "Ardumuo","C++","C#"]
+const carros=["polo","Tcross","Dix","Argo","Cromos"]
+
+export {cursos}
