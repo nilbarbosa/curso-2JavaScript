5e8a95afee7d4af951fc988023e4b46bb95fcc8e
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-12 16:23:21 -0300

    criei segundo exercício sobre animação referente a aula 89

---
+++ aulas/aula89.js
@@ -0,0 +1,30 @@
+const carro=document.getElementById("carro")
+const btn_esquerda=document.getElementById("btn_esquerda")
+const btn_direita=document.getElementById("btn_direita")
+const btn_parar=document.getElementById("parar")
+
+const init=()=>{
+    carro.style="position:relative;left:0px"
+}
+
+let anima=null
+
+const move=(dir)=>{
+    carro.style.left=parseInt(carro.style.left) + (10*dir) +"px"
+}
+
+btn_parar.addEventListener("click",()=>{
+   clearInterval(anima)
+  }) 
+
+btn_esquerda.addEventListener("click",()=>{
+    clearInterval(anima)
+   anima=setInterval(move,20,-1)
+ }) 
+
+btn_direita.addEventListener("click",()=>{
+    clearInterval(anima)
+    anima=setInterval(move,20,1)
+})
+
+window.onload=init
