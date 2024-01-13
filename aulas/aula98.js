b3f8c837ef00169206e2dad745d65e48840953eb
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-13 09:15:41 -0300

    criei exercicio sobre uso de ponto e virgula referente a aula 98

---
+++ aulas/aula98.js
@@ -0,0 +1,23 @@
+let i=null;i=0
+console.log(i)
+//quando possui mais de uma função na mesma linha, é obrigatório o uso de ponto e virgula.
+
+
+let canal="CFB cursos";
+['a','b','c','d','e','f','g','h'].forEach(a=>console.log(a));
+//obrigatório o uso de ; sempre que a próxima linha começar com cochetes ou parenteses.
+
+let n1=[1,2]
+let n2=[3,4];
+[n1,n2].map(v=>console.log(v))
+
+let n=null;
+(n=10)
+console.log(n)
+
+let numeros=[10,20,30]
+let [b1,b2,b3]=numeros
+console.log(b1)
+console.log(b2)
+console.log(b3)
+//ao declarar a váriavel não é necessario uso de cochetes e de ;.
