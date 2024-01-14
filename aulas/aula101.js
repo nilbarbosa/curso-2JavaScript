4c3b3ad90c86dd3d5a6ee012cf73c4e6a56a5aed
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-13 21:46:44 -0300

    criei exercícios de WebStorage referente a aula 101

---
+++ aulas/aula101.js
@@ -0,0 +1,25 @@
+const f_texto=document.querySelector("#f_texto")
+const p_texto=document.querySelector("#p_texto")
+const btn_texto=document.querySelector("#btn_texto")
+
+btn_texto.addEventListener("click",(evt)=>{
+
+})
+
+let num=10
+const idade=17
+//window.localStorage.setItem("numero",num)
+//localStorage.setItem("nome","Nilcimar")
+//localStorage.setItem("idade",idade)
+//localStorage.setItem("numero",num)
+/* alert(localStorage.getItem("nome"))
+alert(localStorage.getItem("idade"))
+alert(localStorage.getItem("numero"))
+localStorage.clear() */
+//alert(localStorage.length)
+//alert(localStorage.getItem(localStorage.key((2))))
+localStorage.clear()
+
+sessionStorage.setItem("nome","Nilcimar")
+sessionStorage.setItem("idade",idade)
+sessionStorage.setItem("numero",num)
