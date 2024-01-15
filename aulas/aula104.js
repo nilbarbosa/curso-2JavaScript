b89846e18828ca122b1075bb7f180d751b93a11e
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-15 16:51:44 -0300

    criei segunda parte do cronômetro referente a aula 104

--- aulas/aula104.js
+++ aulas/aula104.js
@@ -5,8 +5,17 @@ const tmpIni=Date.now()
 const contador=()=>{
     const tmpAtual=Date.now()
     let cont=tmpAtual-tmpIni
-    let seg=Math.floor((tmpAtual-tmpIni)/1000)
-    console.log(seg)
+    let seg=((tmpAtual-tmpIni)/1000)
+    timer.innerHTML=converter(seg)
 }
 
+const converter=(seg)=>{
+    const hora=Math.floor(seg/3600)
+    const resto=seg%3600
+    const minuto=Math.floor(resto/60)
+    const segundo=Math.floor(resto%60)
+    const form=(hora<10?"0"+hora:hora)+":"+(minuto<10?"0"+minuto:minuto)+":"+(segundo<10?"0"+segundo:segundo)
+
+    return form
+}
 setInterval(contador,1000)
