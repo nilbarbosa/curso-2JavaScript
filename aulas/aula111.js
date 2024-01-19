f33c505ae3d5e70ce30226de9dc57b38e115fa08
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-19 08:57:28 -0300

    criei primeira parte do exercício de DataGridView

---
+++ aulas/aula111.js
@@ -0,0 +1,11 @@
+
+const configdgv={
+    endpoint:"produtos.json",
+}
+const dgv=(configdgv)=>{
+    fetch(configdgv.endpoint)
+    .then(res=>res.json())
+    .then(res=>{
+     console.log(res)
+})
+}
