e5859a971812db54c4f98e5aa63f6de84c76cc2e
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2024-01-19 10:31:03 -0300

    adicionei os botões de função

--- aulas/aula111.js
+++ aulas/aula111.js
@@ -4,7 +4,8 @@ const configdgv={
     idDestino:"dgvDados",
 }
 const dgv=(configdgv)=>{
-    const dgvDados=document.getElementById(configdgv.idDestino)
+    const dgvDados=document.getElementById(configdgv.idDestino);
+    dgvDados.innerHTML="";
     fetch(configdgv.endpoint)
     .then(res=>res.json())
     .then(res=>{
@@ -13,29 +14,43 @@ const dgv=(configdgv)=>{
             dgvLinha.setAttribute("class","dgvLinha");
 
             const c1=document.createElement("div");
-            c1.setAttribute("class","c1");
+            c1.setAttribute("class","coluna c1");
             c1.innerHTML=el.id;
             dgvLinha.appendChild(c1);
 
             const c2=document.createElement("div");
-            c2.setAttribute("class","c2");
+            c2.setAttribute("class","coluna c2");
             c2.innerHTML=el.produto;
             dgvLinha.appendChild(c2);
 
             const c3=document.createElement("div");
-            c3.setAttribute("class","c3");
+            c3.setAttribute("class","coluna c3");
             c3.innerHTML=el.marca;
             dgvLinha.appendChild(c3);
 
             const c4=document.createElement("div");
-            c4.setAttribute("class","c4");
+            c4.setAttribute("class","coluna c4");
             c4.innerHTML=el.modelo;
             dgvLinha.appendChild(c4);
 
             const c5=document.createElement("div");
-            c5.setAttribute("class","c5");
-            c5.innerHTML="D e V";
+            c5.setAttribute("class","coluna c5");
             dgvLinha.appendChild(c5);
+
+            const imgDelete=document.createElement("img");
+            imgDelete.setAttribute("class","dgvIcone");
+            imgDelete.setAttribute("src","lixeira.svg");
+            c5.appendChild(imgDelete);
+
+            const imgEditar=document.createElement("img");
+            imgEditar.setAttribute("class","dgvIcone");
+            imgEditar.setAttribute("src","editar.svg");
+            c5.appendChild(imgEditar);
+
+            const imgVisualizar=document.createElement("img");
+            imgVisualizar.setAttribute("class","dgvIcone");
+            imgVisualizar.setAttribute("src","visualizar.svg");
+            c5.appendChild(imgVisualizar);
 
             dgvDados.appendChild(dgvLinha);
             
